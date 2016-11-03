203bda1ab2404012efc2c5c6485a3a0ded4363e9
Moovendhan Elanchezhiyan (OSV) <moovendhan.elanchezhiyan.osv@fedex.com>
2016-11-03 15:45:57 -0500

    simplechange

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return (
+        <div> Is it Worked </div>
+    );
+}
+
+ReactDOM.render (<App />, document.getElementById ('app'));
